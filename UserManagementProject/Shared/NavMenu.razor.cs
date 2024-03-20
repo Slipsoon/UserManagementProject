cd054b19b06d11234990d1151c4e7272cfaa8c9a
Sebastian Ślipek <sebastian.slipek@hotmail.com>
2024-03-20 12:03:29 +0100

    add system navbar

---
+++ UserManagementProject/Shared/NavMenu.razor.cs
@@ -0,0 +1,8 @@
+﻿namespace UserManagementProject.Shared;
+
+public partial class NavMenu
+{
+    public int SelectedBookmark { get; set; } = 1;
+
+    private void ChangeSelectedBookmark(int bookmark) => SelectedBookmark = bookmark;
+}
